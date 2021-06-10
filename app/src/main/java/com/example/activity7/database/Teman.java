1682a7126017d1406c7ebb0a42a05545be6db351
DamasHimawan <damas261100@gmail.com>
2021-06-11 03:47:07 +0700

    Activity7&8

---
+++ app/src/main/java/com/example/activity7/database/Teman.java
@@ -0,0 +1,42 @@
+package com.example.activity7.database;
+
+import java.io.Serializable;
+
+public class Teman {
+    String id;
+    String Nama;
+    String Telepon;
+
+    public Teman() {
+    }
+
+    public Teman(String id, String nama, String telepon) {
+        this.id = id;
+        this.Nama = nama;
+        this.Telepon = telepon;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getTelepon() {
+        return Telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        Telepon = telepon;
+    }
+}
